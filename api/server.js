e062c4deff08880d154aa27625d01bda29e1f816
Priyanka Das <priyankastro7@gmail.com>
2023-02-12 22:05:21 +0530

    server.js

--- api/server.js
+++ api/server.js
@@ -1,9 +1,20 @@
+// // 'use strict';
+// const strapi = require('strapi');
+// // const strapiDev=require('./node_modules/@strapi/strapi/lib/commands/develop')
+
+// // if (process.env.DEV_MODE === true) {
+// //   strapiDev();
+// // } else {
+// //   strapi().start();
+// // }
+// strapi().start();
 'use strict';
 const strapi = require('./node_modules/@strapi/strapi/lib/index');
-const strapiDev=require('./node_modules/@strapi/strapi/lib/commands/develop')
+// const strapiDev=require('./node_modules/strapi/lib/commands/develop')
 
-if (process.env.DEV_MODE === true) {
-  strapiDev();
-} else {
-  strapi().start();
-}
+// if (process.env.DEV_MODE === true) {
+//   strapiDev();
+// } else {
+//   strapi().start();
+// }
+strapi().start();
