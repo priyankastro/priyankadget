25c3c0022d0e0dc1646347e552480fa6e6a15cb6
Priyanka Das <priyankastro7@gmail.com>
2023-02-27 22:03:57 +0530

    server js url update

--- api/config/server.ts
+++ api/config/server.ts
@@ -1,6 +1,9 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  // host: env('HOST', '0.0.0.0'),
+  // port: env.int('PORT', 1337),
+  // url: 'https://priyankadget-api.vercel.app/',
+  url: '/',
+  serveAdminPanel: false,
   app: {
     keys: env.array('APP_KEYS'),
   },
