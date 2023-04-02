77988096683cf11d84bb350fda18e1b0aba69ba5
Priyanka Das <priyankastro7@gmail.com>
2023-04-03 01:33:01 +0530

    form width edit

--- ui/src/app/connect/page.tsx
+++ ui/src/app/connect/page.tsx
@@ -6,7 +6,7 @@ const Connect = ({ data }: any) => {
             <h3 className='font-bold tracking-widest'>Get in touch with me to discuss your project, ask a question or just say hello!</h3>
             <h1 className='multicolored-text heading-xl mb-8 section-heading'>Contact Me!</h1>
             <div className="grid gap-16 grid-cols-1 lg:grid-cols-2">
-                <div className={`w-[500px] m-auto mb-14 lg:mb-auto`}>
+                <div className={`w-full lg:w-[500px] m-auto mb-14 lg:mb-auto`}>
                     <form className="w-full">
                         <div className="flex flex-wrap -mx-3 mb-6">
                             <div className="w-full md:w-1/2 px-3 mb-4 md:mb-0">
