670766309bee2b2942635c713941b99e2a95d859
Priyanka Das <priyankastro7@gmail.com>
2023-04-05 17:39:48 +0530

    button design modified

--- ui/src/app/connect/page.tsx
+++ ui/src/app/connect/page.tsx
@@ -5,10 +5,10 @@ const Connect = ({ data }: any) => {
     return (
         <div>
             <h3 className='font-bold tracking-widest'>Get in touch with me to discuss your project, ask a question or just say hello.</h3>
-            <h1 className='multicolored-text heading-xl mb-8 section-heading'>Contact Me!</h1>
             <div className="grid gap-16 grid-cols-1 lg:grid-cols-2">
                 <div className={`w-full lg:w-[500px] m-auto mb-auto`}>
                     <form className="w-full">
+                        <h1 className='multicolored-text heading-xl mb-8 section-heading'>Contact Me!</h1>
                         <div className="flex flex-wrap -mx-3 mb-6">
                             <div className="w-full md:w-1/2 px-3 mb-4 md:mb-0">
                                 <input className="appearance-none block w-full input-bg py-3 px-4 focus:outline-none" id="name" type="text" placeholder="Full Name" />
@@ -39,8 +39,9 @@ const Connect = ({ data }: any) => {
                         </div>
                     </form>
                 </div>
-                <div className={`w-full lg:w-[500px] m-auto mb-14 lg:mb-auto mt-0`}>
-                    <Image className="m-auto mt-[-30x] lg:mt-[-35px]" src={'/images/contact.png'} alt={'Let me know your thoughts'} width={400} height={400}></Image>
+                <div className={`w-full lg:w-[500px] m-auto mb-14 lg:mb-auto lg:mt-14 mt-0`}>
+                    <Image className="m-auto" src={'/images/contact.png'} alt={'Let me know'} width={400} height={400}></Image>
+                    <h2 className='multicolored-text font-bold heading-lg py-3 text-center'>your thoughts</h2>
                 </div>
             </div>
         </div>
