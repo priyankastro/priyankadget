7adbf9496450e82fbd468d63f3348caa61daaf25
Priyanka Das <priyankastro7@gmail.com>
2023-04-06 01:45:20 +0530

    Suspense added to layout

--- ui/src/app/layoutChild.tsx
+++ ui/src/app/layoutChild.tsx
@@ -3,6 +3,8 @@ import Navbar from "./navbar";
 import { RootState } from './store'
 import { useSelector } from 'react-redux'
 import clsx from "clsx";
+import { Suspense } from "react";
+import Loading from './loading';
 
 const LayoutChild = (props: any) => {
     const collapse = useSelector((state: RootState) => state.global.openNav)
@@ -10,15 +12,17 @@ const LayoutChild = (props: any) => {
         <>
             {/* {console.log(collapse)} */}
             <Navbar />
-            <main className={`relative z-10 w-full ${clsx({
-                ['h-screen overflow-auto']: collapse === true
-            })
-                }`}>
-                <div className='content-body p-8 lg:p-20 md:p-10 xl:p-28 2xl:p-40'>
-                    {props.item}
-                </div>
-                <div className='skewpart'></div>
-            </main>
+            <Suspense fallback={<Loading />}>
+                <main className={`relative z-10 w-full ${clsx({
+                    ['h-screen overflow-auto']: collapse === true
+                })
+                    }`}>
+                    <div className='content-body p-8 lg:p-20 md:p-10 xl:p-28 2xl:p-40'>
+                        {props.item}
+                    </div>
+                    <div className='skewpart'></div>
+                </main>
+            </Suspense>
         </>
     )
 }
