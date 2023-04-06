ee984deef04dbaf2d21f6d2bfb364d2b921f015b
Priyanka Das <priyankastro7@gmail.com>
2023-04-06 17:20:13 +0530

    loading as client component

--- ui/src/app/loading.tsx
+++ ui/src/app/loading.tsx
@@ -1,3 +1,4 @@
+'use client'
 export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
   return <h1 className='multicolored-text heading-xl mb-16 section-heading'>Loading</h1>;
