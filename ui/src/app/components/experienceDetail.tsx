6c73b4b4118da9bf58fd751224deed20934af2c0
Priyanka Das <priyankastro7@gmail.com>
2023-04-23 01:20:17 +0530

    key update error fix

--- ui/src/app/components/experienceDetail.tsx
+++ ui/src/app/components/experienceDetail.tsx
@@ -9,8 +9,8 @@ const ExperienceDetail = ({ data }: any) => {
             <h3 className='mb-3'>{data.duration}</h3>
             <ul>
                 {data.description.map((x: any, i: any) => (
-                    <div className='desc-list relative'>
-                        <li className='mb-1 ml-2 pl-3' key={i}>{x}</li>
+                    <div key={i} className='desc-list relative'>
+                        <li className='mb-1 ml-2 pl-3'>{x}</li>
                     </div>
                 ))}
             </ul>
